6196085586ceeb7d012737a6338bf94bb611fb90
c0state <stephen.liu@alumni.stanford.edu>
2014-07-18 08:59:04 -0400

    Set executable mode on scripts

--- docker_image_mono_from_source/run.sh
+++ docker_image_mono_from_source/run.sh
@@ -1,1 +1,1 @@
-docker build  --no-cache=true --force-rm=true --quiet=false -t stephenliu/mono_from_source:0.1.0 .
+docker build --no-cache=true --force-rm=true --quiet=false -t stephenliu/mono_from_source:0.1.0 .
